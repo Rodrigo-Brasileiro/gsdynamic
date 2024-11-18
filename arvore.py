f726e2c770086afe152897827a84497c6b0d62ae
Rodrigo-Brasileiro <rm98952@fiap.com.br>
2024-11-18 10:32:02 -0300

    Paises escritos

---
+++ arvore.py
@@ -0,0 +1,87 @@
+# {“pais”:”Brasil”, “população”: 200000000, “pib”: xxx, “porcentagem de energia eólica”: yyy, …}
+# Todos os dados foram pegos dos sites desponibilizados na tarefa, mas vou comentá-los aqui
+# Para a população: https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population
+# Para o PIB eu peguei somente o conceito do FMI, aqui está: https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)_per_capita
+# Para energia eólica, solar e nuclear utilizei o mesmo que entregava o percentual de todas por país: https://en.wikipedia.org/wiki/List_of_countries_by_electricity_production
+
+paises = {"pais":"", "população":,
+          "pib":,"energia_eólicaTWH": , "energia_nuclearTWH":,"energia_solarTWH":}
+
+paises = {"pais":"Brasil", "população":203080756,
+          "pib":10296,"energia_eólicaTWH":  82, "energia_nuclearTWH":15,"energia_solarTWH":30}
+ 
+França
+{"pais":"França", "população":68534000,
+          "pib":48012,"energia_eólicaTWH":39, "energia_nuclearTWH":295,"energia_solarTWH":20}
+ 
+Portugal
+{"pais":"Portugal", "população":10639726,
+          "pib":29341,"energia_eólicaTWH":13, "energia_nuclearTWH":0,"energia_solarTWH":4}
+
+Espanha
+{"pais":"Espanha", "população":48946035,
+          "pib":35789,"energia_eólicaTWH": 62, "energia_nuclearTWH":,59"energia_solarTWH":36}
+
+Estados Unidos
+{"pais":"Estados_Unidos", "população":335893238,
+          "pib":86601,"energia_eólicaTWH":434 , "energia_nuclearTWH":772,"energia_solarTWH":205}
+
+India
+{"pais":"India", "população":1404910000,
+          "pib":2698,"energia_eólicaTWH":70 , "energia_nuclearTWH":46,"energia_solarTWH":95}
+
+Alemanha 
+{"pais":"Alemanha ", "população":84708010,
+          "pib":55521,"energia_eólicaTWH":125, "energia_nuclearTWH":35,"energia_solarTWH":61}
+
+China
+{"pais":"China", "população":1409670000,
+          "pib":12969,"energia_eólicaTWH": 2099, "energia_nuclearTWH":2640,"energia_solarTWH":1323}
+
+Japão
+{"pais":"Japão", "população":123790000,
+          "pib":32859,"energia_eólicaTWH":8, "energia_nuclearTWH":52,"energia_solarTWH":102}
+ 
+México
+{"pais":"México", "população":129940228,
+          "pib":13972,"energia_eólicaTWH":20 , "energia_nuclearTWH":11,"energia_solarTWH":19}
+
+Argentina
+{"pais":"Argentina", "população":47067441,
+          "pib":12814,"energia_eólicaTWH": 14, "energia_nuclearTWH":7,"energia_solarTWH":3}
+
+Bélgica
+{"pais":"Bélgica", "população":11846626,
+          "pib":56129,"energia_eólicaTWH": 12, "energia_nuclearTWH":44,"energia_solarTWH":7}
+ 
+Suíça 
+{"pais":"Suíça ", "população":9002763,
+          "pib":106098,"energia_eólicaTWH":0.2, "energia_nuclearTWH":24,"energia_solarTWH":3}
+
+Itália 
+{"pais":"Itália", "população":58968499,
+          "pib":40287,"energia_eólicaTWH": 20, "energia_nuclearTWH":0,"energia_solarTWH":28}
+
+Coreia do Sul
+{"pais":"Coreia do Sul", "população":51248233,
+          "pib":36132,"energia_eólicaTWH":3, "energia_nuclearTWH":176,"energia_solarTWH":27}
+
+Canada
+{"pais":"Canada", "população":41288599,
+          "pib":53834,"energia_eólicaTWH": 38, "energia_nuclearTWH":87,"energia_solarTWH":6} 
+
+Polônia
+{"pais":"Polônia", "população":37543000,
+          "pib":23563,"energia_eólicaTWH":20, "energia_nuclearTWH":0,"energia_solarTWH":8} 
+
+Africa do sul 
+{"pais":"Africa do sul", "população":63015904,
+          "pib":6377,"energia_eólicaTWH": 10, "energia_nuclearTWH":10,"energia_solarTWH":10}
+
+Chile
+{"pais":"Chile", "população":20086377,
+          "pib":16365,"energia_eólicaTWH":9, "energia_nuclearTWH":0,"energia_solarTWH":15}
+
+Finlândia
+{"pais":"Finlândia", "população":5628931,
+          "pib":54774,"energia_eólicaTWH":12, "energia_nuclearTWH":25,"energia_solarTWH":0.01}
