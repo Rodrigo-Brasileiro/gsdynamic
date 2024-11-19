20e385071893647f28325888ab06a9579cfdb5cd
Rodrigo-Brasileiro <rm98952@fiap.com.br>
2024-11-19 18:05:30 -0300

    Commit "Comentário terminado"

--- Ogrande.py
+++ Ogrande.py
@@ -25,7 +25,7 @@ returno2 = conta_letras(palavra2)
 print(returno2)
 
 
-# Se implementada, a a função O(n) deverá ter um consumo energético menor que a função de complexidade O(2n), uma vez que
+# Se implementada, a função O(n) deverá ter um consumo energético menor que a função de complexidade O(2n), uma vez que
 # para uma lista de 1000 elementos, a função de complexidade O(n) realiza 1000 operações para chegar ao fim da lista, enquanto a função O(2n)
 # realiza 2000 operações até chegar ao final da mesma lista, resultando em um consumo energético maior, visto que ela levará
 # o dobro do tempo para ser concluída. Logo, a proporção delas 1:2.
