385265bfce84e95a3cabca5b4801808d99964bf2
Rodrigo-Brasileiro <rm98952@fiap.com.br>
2024-11-19 17:47:46 -0300

    Commit 2a funcao feita

--- Ogrande.py
+++ Ogrande.py
@@ -1,3 +1,5 @@
+
+#Função de complexidade O(n)
 def conta_letras (palavra):
     conta = {}
     for letra in palavra:
@@ -8,3 +10,19 @@ def conta_letras (palavra):
 palavra = "Salame"
 returno = conta_letras(palavra)
 print(returno)
+
+#Função de Complexidade O(2n)
+def conta_letras_O2n(palavra):
+    conta = {}
+    #
+    for letra in palavra:
+        if letra not in conta:
+            conta[letra] = 0
+
+    for letra in palavra:
+        conta[letra] += 1
+
+    return conta
+
+# Exemplo de uso
+print(conta_letras_O2n("Salame"))
